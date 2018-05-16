68c60fad2cb238bfd40c1d9fcc078bb87b3411e5
DIARY-PC\程达瑞 <383001400@qq.com>
2018-05-16 19:34:58 +0800

    First commit

---
+++ Assets/Script/BgControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgControl : MonoBehaviour {
+
+    private float offset = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(float dir)
+    {
+        if(dir == 1)
+        {
+            GetComponent<Renderer>().material.SetTextureOffset("MainTexture", new Vector2(offset += 0.4f * Time.deltaTime, 0));
+        }
+        else
+        {
+            GetComponent<Renderer>().material.SetTextureOffset("MainTexture", new Vector2(offset -= 0.4f * Time.deltaTime, 0));
+        }
+    }
+}
